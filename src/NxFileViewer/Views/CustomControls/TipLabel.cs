6ba2dde075141043d6f738b90dd252f7dad8f851
Myster-Tee <emignatik@gmail.com>
2022-03-25 09:21:30 +0100

    Tooltips can be opened in a Window

--- src/NxFileViewer/Views/CustomControls/TipLabel.cs
+++ src/NxFileViewer/Views/CustomControls/TipLabel.cs
@@ -1,14 +1,19 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
 using Emignatik.NxFileViewer.Localization;
+using Emignatik.NxFileViewer.Utils.MVVM;
 using Emignatik.NxFileViewer.Utils.MVVM.Commands;
+using Emignatik.NxFileViewer.Views.Windows;
 
 namespace Emignatik.NxFileViewer.Views.CustomControls;
 
 public class TipLabel : Label
 {
+    private TipWindow? _tipWindow;
+    private Window? _attachedWindow;
 
     public TipLabel()
     {
@@ -38,19 +43,58 @@ public class TipLabel : Label
     {
         base.OnMouseDown(e);
         e.Handled = true;
+
+        var tipText = TipText;
+        if (tipText == null)
+            return;
+
+        if (_tipWindow == null)
+        {
+            _tipWindow = new TipWindow
+            {
+                Owner = GetAttachedWindow(),
+            };
+            _tipWindow.Closed += OnTipWindowClosed;
+        }
+
+        var pointToScreen = this.PointToScreen(new Point());
+
+        _tipWindow.Top = pointToScreen.Y;
+        _tipWindow.Left = pointToScreen.X;
+
+        _tipWindow.TextBox.Text = tipText;
+
+        _tipWindow.Show();
+        _tipWindow.Activate();
+    }
+
+    private Window? GetAttachedWindow()
+    {
+        return _attachedWindow ??= this.FindAttachedWindow();
+    }
+
+
+    private void OnTipWindowClosed(object? sender, EventArgs e)
+    {
+        if (_tipWindow != null)
+            _tipWindow.Closed -= OnTipWindowClosed;
+        _tipWindow = null;
     }
 
     private void CopyToolTipText()
     {
-        if (this.ToolTip is not string toolTipText) 
+        var tipText = TipText;
+        if (tipText == null)
             return;
         try
         {
-            Clipboard.SetText(toolTipText);
+            Clipboard.SetText(tipText);
         }
         catch
         {
             // ignored
         }
     }
+
+    public string? TipText => this.ToolTip as string;
 }
---
+++ src/NxFileViewer/Views/Windows/TipWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Emignatik.NxFileViewer.Views.Windows
+{
+    /// <summary>
+    /// Logique d'interaction pour TipWindow.xaml
+    /// </summary>
+    public partial class TipWindow : Window
+    {
+        public TipWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
